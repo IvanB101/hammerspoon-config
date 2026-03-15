ea166d64fa0986625c33f7e7c256d555f13c89da
Ivan Brocas <ivan.brocas@feverup.com@Ivans-MacBook-Pro.local>
2026-03-15 18:25:49 +0100

    added dock navigation

---
+++ dock.lua
@@ -0,0 +1,23 @@
+local M = {}
+
+local app = require("hs.application")
+local aux_ui_elem = require("hs.axuielement")
+
+M.open_item = function(n)
+    print("navigating dock")
+	local dock = app.get("Dock")
+	if not dock then
+		return
+	end
+
+	local axDock = aux_ui_elem.applicationElement(dock)
+	local list = axDock[1] -- The main list of icons
+
+	local items = list:attributeValue("AXChildren")
+
+	if items and items[n] then
+		items[n]:performAction("AXPress")
+	end
+end
+
+return M
--- init.lua
+++ init.lua
@@ -1,5 +1,21 @@
 require("auto-reload").init()
 
+local hotkey = require("hs.hotkey")
+local window = require("hs.window")
+
+local dock = require("dock")
+
+local nav_layer = {}
+for _, key in ipairs({ "h", "j", "k", "l" }) do
+	nav_layer[key] = { key = key, modifiers = { "ctrl" } }
+end
+for i = 1, 10 do
+	local key = i - 1
+	nav_layer[tostring(key)] = function()
+		dock.open_item(i)
+	end
+end
+
 require("overload"):init({
 	{
 		key = "escape",
@@ -13,13 +29,15 @@ require("overload"):init({
 	},
 	{
 		key = "tab",
-		layer = {
-			h = { key = "h", modifiers = { "ctrl" } },
-			j = { key = "j", modifiers = { "ctrl" } },
-			k = { key = "k", modifiers = { "ctrl" } },
-			l = { key = "l", modifiers = { "ctrl" } },
-			-- TODO: navigation
-		},
+		layer = nav_layer,
 		ignore = { "ctrl", "cmd", "shift" },
 	},
 })
+
+hotkey.bind({ "cmd" }, "k", function()
+	local win = window.focusedWindow()
+	if not win then
+		return
+	end
+	win:maximize()
+end)
